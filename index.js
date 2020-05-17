ee26a766e4ab1102044566b79fe3951c9ba87727
Neeraj <neeraj.nathany@gmail.com>
2020-05-17 21:12:10 +0530

    Make api request on get, instead of on post, increasing speed

--- index.js
+++ index.js
@@ -10,31 +10,36 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 const url = 'http://api.currencylayer.com/live?access_key=b22031f591ef1848e3229c6e23c73712';
 const source = 'USD';   //Unchageable in the basic api plan
+const currency = 'INR';
+var currRate;
+var showCurrency;
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
-})
-
-app.post('/', function(req,res){
-  var currency = 'INR';
   var final = url+'&source='+source+'&currencies='+currency;
-  var value = req.body.value;
   http.get(final, function(response){
     response.on('data', function(data){
       var out = JSON.parse(data);
-      if (req.body.currency == 'INR'){
-        var ans = value / out.quotes[source+currency];
-        currency = 'USD';
-      }
-      else{
-        var ans = out.quotes[source+currency]*value;
-      }
-      var output = (Math.round((ans+Number.EPSILON)*100)/100).toLocaleString()
-      //res.render("That's approximately "+ output + ' ' + currency);
-      res.render('output', {output:output, currency:currency});
+      currRate = out.quotes[source+currency];
     })
   })
 })
+
+app.post('/', function(req,res){
+  var value = req.body.value;
+  if (req.body.currency == 'INR'){
+    var ans = value / currRate;
+    showCurrency = 'USD';
+  }
+  else{
+    var ans = currRate*value;
+    showCurrency = 'INR';
+  }
+  var output = (Math.round((ans+Number.EPSILON)*100)/100).toLocaleString()
+  res.render('output', {output:output, currency:showCurrency});
+})
+
+
 
 let port = process.env.PORT || 3000;
 
